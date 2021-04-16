9071532d402c87c2679ee0aa46b4dc499a528263
Ubuntu <ubuntu@vmlab-ogg-13.us-east1-c.c.copa-infra-east-gcp.internal>
2021-04-16 22:33:35 +0000

    sesion 2 terraform

---
+++ network.tf
@@ -0,0 +1,41 @@
+# Crea red virtual
+resource "azurerm_virtual_network" "vn" {
+    name                = var.vnet_name
+    address_space       = var.vnet_cidr
+    location            = var.region
+    resource_group_name = azurerm_resource_group.rg.name
+
+    tags = {
+        environment = var.initiative
+    }
+}
+
+resource "azurerm_subnet" "subnet_1" {
+    name                 = var.subnet_1_name
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vn.name
+    address_prefixes     = var.subnet_1_ipam
+}
+
+
+resource "azurerm_subnet" "subnet_2" {
+    name                 = var.subnet_2_name
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vn.name
+    address_prefixes     = var.subnet_2_ipam
+}
+
+
+resource "azurerm_subnet" "subnet_3" {
+    name                 = var.subnet_3_name
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vn.name
+    address_prefixes     = var.subnet_3_ipam
+}
+
+resource "azurerm_subnet" "subnet_4" {
+    name                 = var.subnet_4_name
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vn.name
+    address_prefixes     = var.subnet_4_ipam
+}
